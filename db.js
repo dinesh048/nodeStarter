7b8c0afd25f236e1d86254aeecc8e0d1160fbb37
Dinesh Regmi <dinesh.regmi@8squarei.com>
2024-11-28 21:47:17 +0545

    setup

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/demo");
+mongoose.connect("connection string");
 const db = mongoose.connection;
 db.on("error", () => {
   console.log("error");
